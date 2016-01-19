dbcd70c0f1985d80e1dcb77158905dfe7e8238c5
aijingyc <aijingyc@gmail.com>
2016-01-19 07:39:41 +0000

    Use [] to dereference a pointer in Exercise 11

--- kern/monitor.c
+++ kern/monitor.c
@@ -68,12 +68,12 @@ mon_backtrace(int argc, char **argv, struct Trapframe *tf)
 	ebp = read_ebp();
 	pebp = (uint32_t *) ebp;
 	while (pebp) {
-		eip = *(pebp + 1);
+		eip = pebp[1];
 		cprintf("  ebp %08x eip %08x ", ebp, eip);
 	
 		cprintf("args ");
 		for (i = 1; i <= 5; i++) {
-			cprintf("%08x ", *(pebp + 1 + i));
+			cprintf("%08x ", pebp[1 + i]);
 		}
 		cprintf("\n");
 
