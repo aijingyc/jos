53af7623bd9d81e476e48fc573269c7aa042ed3f
aijingyc <aijingyc@gmail.com>
2016-01-27 08:06:59 +0000

    fix dumpped vm format in mon_dumpvm command

--- kern/monitor.c
+++ kern/monitor.c
@@ -213,7 +213,7 @@ mon_dumpvm(int argc, char **argv, struct Trapframe *tf)
 		if (!pte || !(*pte & PTE_P))
 			cprintf("virtual address 0x%x is not mapped yet\n", va);
 		else
-			cprintf("virtual address 0x%x: %c\n", va, *va);
+			cprintf("virtual address 0x%x: 0x%02x\n", va, (uint8_t) *va);
 
 		if (va + 1 < va)
 			break;
