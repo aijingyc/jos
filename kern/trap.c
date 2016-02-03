1d12fabede4014fc8faf56654cc60d1f8cafb98a
Jing Ai <aijingyc@gmail.com>
2016-02-03 04:22:03 +0000

    finish the leftover in lab3

--- kern/trap.c
+++ kern/trap.c
@@ -230,6 +230,10 @@ page_fault_handler(struct Trapframe *tf)
 	// Handle kernel-mode page faults.
 
 	// LAB 3: Your code here.
+	if (!(tf->tf_cs & 3)) {
+		print_trapframe(tf);
+		panic("page fault happened in kernel mode");
+	}
 
 	// We've already handled kernel-mode exceptions, so if we get here,
 	// the page fault happened in user mode.
