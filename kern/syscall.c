b50d4ffe5496ae363ac610d488875181d9c3f9f9
Jing Ai <aijingyc@gmail.com>
2016-02-03 07:17:14 +0000

    Excercise 10: implemented _pgfault_upcall

--- kern/syscall.c
+++ kern/syscall.c
@@ -372,7 +372,7 @@ syscall(uint32_t syscallno, uint32_t a1, uint32_t a2, uint32_t a3, uint32_t a4, 
 	case SYS_env_set_status:
 		return sys_env_set_status(a1, a2);
 	case SYS_env_set_pgfault_upcall:
-		sys_env_set_pgfault_upcall(a1, (void *) a2);
+		return sys_env_set_pgfault_upcall(a1, (void *) a2);
 	case SYS_yield:
 		sys_yield();
 		return 0;
