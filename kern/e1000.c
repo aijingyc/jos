e2801a99cc173bc32d9ab6ffc25c6ba48ed5de86
aijingyc <aijingyc@gmail.com>
2016-02-07 07:09:03 +0000

    Excercise 3: implemented the skeleton on attaching E1000 as a PCI device to PCI bus

--- kern/e1000.c
+++ kern/e1000.c
@@ -1,3 +1,12 @@
 #include <kern/e1000.h>
 
 // LAB 6: Your driver code here
+#include <kern/pci.h>
+
+int
+e1000_attach(struct pci_func *pcif)
+{
+	pci_func_enable(pcif);
+	return 0;
+}
+
--- kern/e1000.h
+++ kern/e1000.h
@@ -1,4 +1,11 @@
 #ifndef JOS_KERN_E1000_H
 #define JOS_KERN_E1000_H
 
+#define E1000_VENDOR_ID 0x8086
+#define E1000_DEVICE_ID 0x100e
+
+struct pci_func;
+
+int e1000_attach(struct pci_func *f);
+
 #endif	// JOS_KERN_E1000_H
