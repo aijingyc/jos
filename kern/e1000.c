cb6ffb70986eb2d7c952f97841da0688cb394805
aijingyc <aijingyc@gmail.com>
2016-02-09 06:58:14 +0000

    added e1000_write_reg() helper function and transmission related marcos

--- kern/e1000.c
+++ kern/e1000.c
@@ -20,12 +20,26 @@
 #define E1000_CTRL_DUP 0x00004  /* Device Control Duplicate (Shadow) - RW */
 #define E1000_STATUS   0x00008  /* Device Status - RO */
 
+#define E1000_TCTL     0x00400  /* TX Control - RW */
+#define E1000_TDBAL    0x03800  /* TX Descriptor Base Address Low - RW */
+#define E1000_TDBAH    0x03804  /* TX Descriptor Base Address High - RW */
+#define E1000_TDLEN    0x03808  /* TX Descriptor Length - RW */
+#define E1000_TDH      0x03810  /* TX Descriptor Head - RW */
+#define E1000_TDT      0x03818  /* TX Descripotr Tail - RW */
+#define E1000_TXDCTL   0x03828  /* TX Descriptor Control - RW */
+
 volatile uint32_t *e1000;
 
 static uint32_t
 e1000_read_reg(uint32_t offset)
 {
 	return e1000[offset / sizeof(uint32_t)];
+}
+
+static void
+e1000_write_reg(uint32_t offset, uint32_t value)
+{
+	e1000[offset / sizeof(uint32_t)] = value;
 }
 
 int
